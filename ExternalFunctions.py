edf68b02008adeef92028e8a4438df897000a215
Siddharth <siddharthghantasala@gmail.com>
2026-05-30 18:48:57 +0530

    Changed up the activation functions to return the error instead of just the activated version of the outputs

--- ExternalFunctions.py
+++ ExternalFunctions.py
@@ -1,20 +1,24 @@
 import numpy as np
 
-def sigmoid(x : np.ndarray, derivative : bool = False) -> int:
+# Activation Functions
+def sigmoid(x : np.ndarray, upstream_gradient, derivative : bool = False) -> int:
     if derivative:
-        return sigmoid(x, False) * (1 - sigmoid(x, False))
-    return 1.0/(1.0 + np.exp(-x))
+        return sigmoid(x, upstream_gradient,False) * (1 - sigmoid(x, upstream_gradient, False))
+    return upstream_gradient * 1.0/(1.0 + np.exp(-x))
 
-def relu(x : np.ndarray, derivative : bool = False) -> int:
+def relu(x : np.ndarray, upstream_gradient, derivative : bool = False) -> int:
     if derivative:
-        return np.where(x > 0, 1, 0)
-    return np.where(x > 0, x, 0)
+        return upstream_gradient * np.where(x > 0, 1, 0)
+    return upstream_gradient * np.where(x > 0, x, 0)
 
-def softmax(x : np.ndarray, temperature=1.0) -> int:
-    e_x = np.exp((x - np.max(x)) / temperature)
-    return e_x / e_x.sum(axis=0)
+def softmax(x : np.ndarray, derivative : bool, upstream_gradient, temperature=1.0) -> int:
+    e_x = np.exp((x - np.max(x, axis=0, keepdims=True)) / temperature)
+    s = e_x / e_x.sum(axis=0)
+    if derivative:
+        return s * (upstream_gradient - np.sum(s * upstream_gradient, axis=0, keepdims=True))
+    return s
 
-def tanh(x : np.ndarray, derivative : bool = False) -> int:
+def tanh(x : np.ndarray, upstream_gradient, derivative : bool = False) -> int:
     if derivative:
         return upstream_gradient * (1 - np.tanh(x) ** 2)
     return upstream_gradient * np.tanh(x)
